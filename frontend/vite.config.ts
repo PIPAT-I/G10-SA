974d8ba08b3a90df9beff08f34599c5d2e6eb42c
laziesv <laziesv@gmail.com>
2025-08-30 13:42:01 +0700

    Poopha

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,        // กำหนด port เป็น 3000
+    port: 3001,        // กำหนด port เป็น 3000
     strictPort: true,  // ถ้า port ไม่ว่าง จะ error แทนที่จะหา port อื่น
     open: true         // เปิด browser อัตโนมัติ
   }
